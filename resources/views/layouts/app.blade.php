ffd7e7f735116c7d3ac9d768d2cf485ca0dbf0f6
Rinku-gahalout <rinkugahalout012@gmail.com>
2025-07-18 12:21:36 +0530

    service page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,9 +9,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- for social-links -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+
 
     <!-- Custom CSS -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/service_style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/blog_style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/contact_style.css') }}">
 
 </head>
 <body>
